337dbece2596566811ef8e612c4d615a2297b3ba
Delia Constantino <dconstan92@gmail.com>
2020-10-27 22:07:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,8 @@ def line(line_array)
     if line_length = 0
       puts "The line is currently empty."
     else
-      count = 0
-      line_array.each do |name|
-        
+      line_array.map.with_index
+
       end
     end
 end
