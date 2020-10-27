6999dea07b1857c97585fc7184047aef81b56205
Delia Constantino <dconstan92@gmail.com>
2020-10-27 22:19:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 
 
-def line(line_array)
-  line_length = line_array.size
+def line(katz_deli)
+  line_length = katz_delisize
     if line_length == 0
       puts "The line is currently empty."
     else
-      line_order = line_array.map.with_index{|name, i| "#{i + 1}. #{name}"}
+      line_order = katz_deli.map.with_index{|name, i| "#{i + 1}. #{name}"}
       puts "The line is currently: #{line_order.join(" ")}"
     end
 end
+
