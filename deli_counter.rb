341487b638bd858633264074821d34576bde2037
Delia Constantino <dconstan92@gmail.com>
2020-10-27 21:59:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = []
 
 def line(line_array)
-line_length = line_array.length
-  if line_length = 0
-    puts "The line is currently empty."
-  else
-  end
+  line_length = line_array.length
+    if line_length = 0
+      puts "The line is currently empty."
+    else
+    end
 end
