6652a76be4b9d07ca1177ab7ba82c49b0e95efcd
Delia Constantino <dconstan92@gmail.com>
2020-10-27 22:16:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 
 def line(line_array)
   line_length = line_array.size
-    if line_length = 0
+    if line_length == 0
       puts "The line is currently empty."
     else
       line_order = line_array.map.with_index{|name, i| "#{i + 1}. #{name}"}
