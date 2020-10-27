ea9493df47eb8faffe9caed34c4483c762d5a39e
Delia Constantino <dconstan92@gmail.com>
2020-10-27 22:14:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(line_array)
     if line_length = 0
       puts "The line is currently empty."
     else
-      line_order = line_array.map.with_index{|name, i| {"#{i + 1}. #{name}"}
+      line_order = line_array.map.with_index{|name, i| "#{i + 1}. #{name}"}
       puts "The line is currently: #{line_order}"
     end
 end
